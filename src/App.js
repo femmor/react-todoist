43a2fbb87b9e88e72fed45f8eec0ceb99ec3504e
Emmanuel Egomson <fegomson@gmail.com>
2021-04-20 12:06:38 -0500

    Building the Header Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+// Components
+import { Header } from "./components/Layouts/Header"
+
 export const App = () => {
   return (
     <div className="App">
       <h1>React App</h1>
+      <Header/>
     </div>
   );
 }

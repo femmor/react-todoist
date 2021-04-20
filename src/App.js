373c31ab22252ded78723169e6d2650f776392a4
Emmanuel Egomson <fegomson@gmail.com>
2021-04-20 14:34:42 -0500

    Building the Content and Sidebar components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 // Components
 import { Header } from "./components/Layouts/Header"
+import { Content } from './components/Layouts/Content';
 
 export const App = () => {
   return (
     <div className="App">
       <h1>React App</h1>
       <Header/>
+      <Content/>
     </div>
   );
 }
---
+++ src/components/Layouts/Content.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {SideBar} from "../Layouts/SideBar"
+
+export const Content = () => {
+  return (
+    <section>
+      <SideBar/>
+      Content
+    </section>
+  )
+}
